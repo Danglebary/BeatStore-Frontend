3b1c28cdee5c16e3be26f63a7a9ce02f35f61cb6
Danglebary <halfblowncontact@gmail.com>
2021-10-01 15:03:58 -0700

    adjusted layout to reflect pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,29 +7,58 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 // GraphQL imports
 import { useBeatsQuery } from "../generated/graphql";
 // Chakra imports
-import { Link } from "@chakra-ui/layout";
+import { Box, Flex, Heading, Link, Stack } from "@chakra-ui/layout";
 // Custom imports
 import { Layout } from "../components/Layout";
+import { Button } from "@chakra-ui/button";
 
 const Index: React.FC<{}> = () => {
-    const [{ data }] = useBeatsQuery({
+    const [{ data, fetching }] = useBeatsQuery({
         variables: {
-            limit: 10
+            limit: 1
         }
     });
+
+    let body = null;
+
+    if (fetching && !data) {
+        body = <div>loading...</div>;
+    } else if (!fetching && !data) {
+        body = <div>could not load beats 😭</div>;
+    } else {
+        body = (
+            <Stack spacing={8}>
+                {data!.beats.map(
+                    ({ title, id, bpm, key, genre, creatorId }) => (
+                        <Box
+                            p={5}
+                            shadow="md"
+                            borderWidth="1px"
+                            key={title + id}
+                        >
+                            <Heading fontSize="lg">{title}</Heading>
+                            <p>{bpm}</p>
+                            <p>{key}</p>
+                            <p>{genre}</p>
+                            <p>{creatorId}</p>
+                        </Box>
+                    )
+                )}
+                {data ? <Button isLoading={fetching}>load more</Button> : null}
+            </Stack>
+        );
+    }
+
     return (
         <Layout varient="regular">
-            <NextLink href="/create-beat">
-                <Link>create beat</Link>
-            </NextLink>
+            <Flex align="flex-end">
+                <Heading>beatStore</Heading>
+                <NextLink href="/create-beat">
+                    <Link ml="auto">create beat</Link>
+                </NextLink>
+            </Flex>
             <br />
-            {!data ? (
-                <div>loading...</div>
-            ) : (
-                data.beats.map(({ title, id }) => (
-                    <div key={title + id}>{title}</div>
-                ))
-            )}
+            {body}
         </Layout>
     );
 };
