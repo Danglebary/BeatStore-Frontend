e5f0c8a67132893c5e63fc9e1d53b8cbaa01cbcc
Danglebary <halfblowncontact@gmail.com>
2021-10-03 11:24:28 -0700

    added "like" button, changed Icons to be IconButtons

--- src/components/BeatCardMain.tsx
+++ src/components/BeatCardMain.tsx
@@ -1,18 +1,21 @@
 // General imports
 import React from "react";
 import NextLink from "next/link";
+// GraphQL type imports
+import { BeatSimpleFragment } from "../generated/graphql";
 // Chakra imports
 import { Box, Flex, Heading, Link, Text } from "@chakra-ui/layout";
-import Icon from "@chakra-ui/icon";
+import { Tooltip } from "@chakra-ui/tooltip";
+import { IconButton } from "@chakra-ui/button";
 import { useDisclosure } from "@chakra-ui/hooks";
 // React Icon imports
 import { AiOutlineInfoCircle } from "react-icons/ai";
+import { HiOutlineHeart } from "react-icons/hi";
 // Custom imports
 import { BeatModal } from "./BeatModal";
-import { Tooltip } from "@chakra-ui/tooltip";
 
 interface BeatCardMainProps {
-    beat: any;
+    beat: BeatSimpleFragment;
 }
 
 export const BeatCardMain: React.FC<BeatCardMainProps> = ({ beat }) => {
@@ -26,20 +29,32 @@ export const BeatCardMain: React.FC<BeatCardMainProps> = ({ beat }) => {
                     <Heading fontSize="x-large">{beat.title}</Heading>
                     <Tooltip label="beat info">
                         <span style={{ marginLeft: "auto" }}>
-                            <Icon
+                            <IconButton
                                 fontSize="x-large"
-                                as={AiOutlineInfoCircle}
+                                aria-label="beat info"
+                                icon={<AiOutlineInfoCircle />}
                                 onClick={onOpen}
                             />
                         </span>
                     </Tooltip>
                 </Flex>
-                <Text>
-                    Prod.{" "}
-                    <NextLink href={`/users/${beat.creator.id}`}>
-                        <Link>{beat.creator.userName}</Link>
-                    </NextLink>
-                </Text>
+                <Flex mt={2}>
+                    <Text>
+                        Prod.{" "}
+                        <NextLink href={`/users/${beat.creator.id}`}>
+                            <Link>{beat.creator.userName}</Link>
+                        </NextLink>
+                    </Text>
+                    <Tooltip label="like this beat?">
+                        <span style={{ marginLeft: "auto" }}>
+                            <IconButton
+                                fontSize="x-large"
+                                aria-label="like this beat"
+                                icon={<HiOutlineHeart />}
+                            />
+                        </span>
+                    </Tooltip>
+                </Flex>
             </Box>
         </>
     );
