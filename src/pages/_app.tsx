e3339215c534d8782f96f4f4c7e14cf571536737
Danglebary <halfblowncontact@gmail.com>
2021-10-02 23:42:00 -0700

    removed colormode provider, as it is now handled in _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
 // Chakra imports
-import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 // Custom imports
 import theme from "../theme";
 
 const MyApp = ({ Component, pageProps }: any) => {
     return (
         <ChakraProvider resetCSS theme={theme}>
-            <ColorModeProvider
-                options={{
-                    useSystemColorMode: true
-                }}
-            >
-                <Component {...pageProps} />
-            </ColorModeProvider>
+            <Component {...pageProps} />
         </ChakraProvider>
     );
 };
