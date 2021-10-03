b5a95626ea777d3e766604a2b101e8962a7b3aa2
Danglebary <halfblowncontact@gmail.com>
2021-10-02 23:39:39 -0700

    created default modal for beats, used when clicking info button on card

---
+++ src/components/BeatModal.tsx
@@ -0,0 +1,55 @@
+// General imports
+import React from "react";
+import NextLink from "next/link";
+// Chakra imports
+import {
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalHeader,
+    ModalOverlay
+} from "@chakra-ui/modal";
+import { Box, Grid, Link, Text } from "@chakra-ui/layout";
+
+interface BeatModalProps {
+    beat: any;
+    isOpen: any;
+    onClose: any;
+}
+
+export const BeatModal: React.FC<BeatModalProps> = ({
+    beat,
+    isOpen,
+    onClose
+}) => {
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader fontSize="x-large">
+                    {beat.title}
+                    <Text fontSize="lg" fontWeight="medium">
+                        Prod.{" "}
+                        <NextLink href={`/users/${beat.creator.id}`}>
+                            <Link onClick={onClose}>
+                                {beat.creator.userName}
+                            </Link>
+                        </NextLink>
+                    </Text>
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Grid width="50%" templateColumns="repeat(2, 1fr)" gap={2}>
+                        <Box>Genre:</Box>
+                        <Box>{beat.genre}</Box>
+                        <Box>Bpm:</Box>
+                        <Box>{beat.bpm}</Box>
+                        <Box>Key:</Box>
+                        <Box>{beat.key}</Box>
+                    </Grid>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+    );
+};
