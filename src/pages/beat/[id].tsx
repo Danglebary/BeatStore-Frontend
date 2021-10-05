9751c9f7435f73074f86ba8c3f6535886558ab1e
Danglebary <halfblowncontact@gmail.com>
2021-10-05 13:39:24 -0700

    added markup for page design, will work on more

--- src/pages/beat/[id].tsx
+++ src/pages/beat/[id].tsx
@@ -4,11 +4,54 @@ import { useRouter } from "next/router";
 // Urql imports
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../../utils/createUrqlClient";
+// GraphQL imports
+import { useBeatQuery } from "../../generated/graphql";
+// Chakra imports
+import { Box, Heading } from "@chakra-ui/layout";
+// Custom imports
+import { Layout } from "../../components/Wrappers/Layout";
 
 const Beat: React.FC = () => {
     const router = useRouter();
-    const id = router.query.id;
-    return <div>beat page for beat with id: {id}</div>;
+    const intId =
+        typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+
+    const [{ data, error, fetching }] = useBeatQuery({
+        pause: intId === -1,
+        variables: {
+            id: intId
+        }
+    });
+
+    let body = null;
+
+    if (fetching) {
+        body = <Box>loading...</Box>;
+    }
+
+    if (error) {
+        body = <Box>{error.message}</Box>;
+    }
+
+    if (!data?.beat) {
+        body = <Box>Could not find beat</Box>;
+    } else {
+        body = (
+            <Box>
+                <Heading>{data.beat.title}</Heading>
+                <Box>{data.beat.creator.username}</Box>
+                <Box>{data.beat.genre}</Box>
+                <Box>{data.beat.bpm}</Box>
+                <Box>{data.beat.key}</Box>
+                <Box>{data.beat.tags}</Box>
+                <Box>{data.beat.likesCount}</Box>
+                <Box>{data.beat.createdAt}</Box>
+                <Box>{data.beat.updatedAt}</Box>
+            </Box>
+        );
+    }
+
+    return <Layout>{body}</Layout>;
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Beat);
