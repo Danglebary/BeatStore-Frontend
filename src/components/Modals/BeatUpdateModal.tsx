95e0c9033b4f4ad50901c3f67bef7804cb37becb
Danglebary <halfblowncontact@gmail.com>
2021-10-06 06:28:55 -0700

    completed BeatUpdateModal component

--- src/components/Modals/BeatUpdateModal.tsx
+++ src/components/Modals/BeatUpdateModal.tsx
@@ -1,9 +1,12 @@
 // General imports
 import React from "react";
+import { useRouter } from "next/router";
 // GraphQL type imports
 import {
     BeatSimpleFragment,
-    useDeleteBeatMutation
+    UpdateBeatInput,
+    useDeleteBeatMutation,
+    useUpdateBeatMutation
 } from "../../generated/graphql";
 // Chakra imports
 import {
@@ -18,6 +21,12 @@ import { IconButton } from "@chakra-ui/button";
 // React-icons imports
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { Tooltip } from "@chakra-ui/tooltip";
+import { Form, Formik } from "formik";
+// Custom imports
+import UpdateBeatFormModal from "../Forms/UpdateBeat/UpdateBeatFormModal";
+import { MusicalKeys } from "../../types/musicalKeysTypes";
+import { Button, Flex } from "@chakra-ui/react";
+import { toErrorMap } from "../../utils/toErrorMap";
 
 interface BeatUpdateModalProps {
     beat: BeatSimpleFragment;
@@ -30,23 +39,69 @@ const BeatUpdateModal: React.FC<BeatUpdateModalProps> = ({
     isOpen,
     onClose
 }) => {
+    const router = useRouter();
+    const [{}, updateBeat] = useUpdateBeatMutation();
     const [{}, deleteBeat] = useDeleteBeatMutation();
 
     const handleDeleteBeat = () => {
         deleteBeat({ id: beat.id });
     };
+    let tagsArray: string[] = [];
+    if (beat.tags) {
+        tagsArray = beat.tags.split(",");
+    }
+
+    const initialValues: UpdateBeatInput = {
+        id: beat.id,
+        title: beat.title,
+        genre: beat.genre ? beat.genre : "",
+        bpm: beat.bpm ? beat.bpm : 0,
+        key: beat.key ? (beat.key as MusicalKeys) : MusicalKeys.C_MAJOR,
+        tags: tagsArray
+    };
 
     return (
-        <Modal isOpen={isOpen} onClose={onClose}>
+        <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader fontSize="x-large">
-                    edit track "{beat.title}""
+                    edit beat: {beat.title}
                 </ModalHeader>
                 <Tooltip label="close modal">
                     <ModalCloseButton />
                 </Tooltip>
-                <ModalBody></ModalBody>
+                <ModalBody>
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={async (values, { setErrors }) => {
+                            const res = await updateBeat({ options: values });
+                            if (res.data?.updateBeat.errors) {
+                                setErrors(
+                                    toErrorMap(res.data.updateBeat.errors)
+                                );
+                            } else if (res.data?.updateBeat.beat) {
+                                router.push(`/beat/${values.id}`);
+                            }
+                        }}
+                    >
+                        {({ values, setFieldValue }: any) => (
+                            <Form>
+                                <Flex
+                                    flexDirection="column"
+                                    justifyContent="center"
+                                    alignItems="center"
+                                >
+                                    <UpdateBeatFormModal
+                                        beat={beat}
+                                        values={values}
+                                        setFieldValue={setFieldValue}
+                                    />
+                                    <Button type="submit">Update</Button>
+                                </Flex>
+                            </Form>
+                        )}
+                    </Formik>
+                </ModalBody>
                 <Tooltip label="delete beat">
                     <IconButton
                         position="absolute"
