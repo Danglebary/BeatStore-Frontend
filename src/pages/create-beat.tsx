01a68493427f439b2e275f00961c3dd96fb4441a
Danglebary <halfblowncontact@gmail.com>
2021-10-07 16:38:55 -0700

    organized imports, added FormProgressStepper to Layout

--- src/pages/create-beat.tsx
+++ src/pages/create-beat.tsx
@@ -1,19 +1,22 @@
 // General imports
 import React, { useState } from "react";
 import { useRouter } from "next/router";
+// Chakra imports
+import { Box, Heading } from "@chakra-ui/layout";
 // Urql imports
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 // GraphQL imports
 import { CreateBeatInput, useCreateBeatMutation } from "../generated/graphql";
 // Custom imports
-import { useIsAuth } from "../utils/useIsAuth";
+import { Layout } from "../components/Wrappers/Layout";
 import StepOne from "../components/Forms/CreateBeat/StepOne";
 import StepTwo from "../components/Forms/CreateBeat/StepTwo";
+import { FormProgressStepper } from "../components/Forms/FormProgressStepper";
 import StepThree from "../components/Forms/CreateBeat/StepThree";
 import { CreateBeatFormDataType } from "../types/createBeatFormInputTypes";
 import { MusicalKeys } from "../types/musicalKeysTypes";
-import { Layout } from "../components/Wrappers/Layout";
+import { useIsAuth } from "../utils/useIsAuth";
 
 const CreateBeat: React.FC = () => {
     useIsAuth();
@@ -59,7 +62,15 @@ const CreateBeat: React.FC = () => {
         <StepThree prev={handlePrevStep} next={handleNextStep} data={data} />
     ];
 
-    return <Layout varient="small">{steps[currentStep]}</Layout>;
+    return (
+        <Layout varient="small">
+            <Heading size="lg" mb={6}>
+                Create new beat
+            </Heading>
+            <FormProgressStepper currentStep={currentStep} steps={steps} />
+            <Box>{steps[currentStep]}</Box>
+        </Layout>
+    );
 };
 
 export default withUrqlClient(createUrqlClient)(CreateBeat);
