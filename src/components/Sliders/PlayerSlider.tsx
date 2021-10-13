882ae06c7c04c31890bebb7dd12a21d9a16699b4
Danglebary <halfblowncontact@gmail.com>
2021-10-12 18:06:58 -0700

    created seekBar for audio player

---
+++ src/components/Sliders/PlayerSlider.tsx
@@ -0,0 +1,50 @@
+// General imports
+import React from "react";
+// Chakra imports
+import {
+    Slider,
+    SliderTrack,
+    SliderFilledTrack,
+    SliderThumb
+} from "@chakra-ui/react";
+
+interface PlayerSliderProps {
+    isPlaying: boolean;
+    toggleAudio: () => void;
+    totalDur: number;
+    currentTime: number;
+    setTime: (val: number) => void;
+}
+
+export const PlayerSlider: React.FC<PlayerSliderProps> = ({
+    isPlaying,
+    toggleAudio,
+    totalDur,
+    currentTime,
+    setTime
+}) => {
+    const handleSliderchange = (val: number) => {
+        const timeToSet: number = Math.round((val / 100) * totalDur);
+        setTime(timeToSet);
+        if (!isPlaying) {
+            toggleAudio();
+        }
+    };
+
+    const sliderTime = Math.round((currentTime / totalDur) * 100);
+
+    return (
+        <Slider
+            aria-label="track seek bar"
+            defaultValue={0}
+            value={sliderTime}
+            onChange={handleSliderchange}
+            focusThumbOnChange={false}
+        >
+            <SliderTrack>
+                <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+        </Slider>
+    );
+};
