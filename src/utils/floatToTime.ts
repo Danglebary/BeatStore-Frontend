89bde1202699879995a16f219bcf64c49fd31e2d
Danglebary <halfblowncontact@gmail.com>
2021-10-12 18:03:43 -0700

    created util function to create min:sec formated time string from float duration

---
+++ src/utils/floatToTime.ts
@@ -0,0 +1,9 @@
+export const floatToTime = (num: number) => {
+    let mins = Math.floor(num / 60);
+    let secs = Math.round(num % 60);
+    let secsStr = secs.toString();
+    if (secsStr.length < 2) {
+        secsStr = "0" + secsStr;
+    }
+    return [mins, secsStr].join(":");
+};
