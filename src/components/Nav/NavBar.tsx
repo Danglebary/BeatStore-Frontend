24d9eac3f5f8e3092ff9369e6078ad1cd8d9af95
Danglebary <halfblowncontact@gmail.com>
2021-10-23 11:09:52 -0700

    refactored styles

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -2,77 +2,49 @@
 import React from "react";
 import NextLink from "next/link";
 // GraphQL imports
-import { useMeQuery, useLogoutMutation } from "../../generated/graphql";
+import { useMeQuery } from "../../generated/graphql";
 // Chakra imports
-import { Flex, Heading, Link } from "@chakra-ui/layout";
+import { Flex, Heading } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/react";
 // Custom imports
 import { isServer } from "../../utils/isServer";
+import { NavLayoutWithAuth } from "./NavLayoutWithAuth";
+import { NavLayoutNoAuth } from "./NavLayoutNoAuth";
+import { useDimensions } from "../../hooks/useDimensions";
 
 export const NavBar: React.FC = () => {
-    const [{ data, fetching: meFetching }] = useMeQuery({
+    const sizeType = useDimensions();
+
+    const [{ data, fetching }] = useMeQuery({
         pause: isServer()
     });
 
-    const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
-
     let body = null;
 
-    if (meFetching) {
-        // data is loading
-    } else if (!data?.me) {
-        // user not logged in
+    if (fetching) {
+    } else if (data && data.me === null) {
+        body = <NavLayoutNoAuth varient={sizeType} />;
+    } else if (data && data.me) {
         body = (
-            <>
-                <NextLink href="/login">
-                    <Button variant="link">login</Button>
-                </NextLink>
-                <NextLink href="/register">
-                    <Button variant="link">register</Button>
-                </NextLink>
-            </>
-        );
-    } else {
-        // user logged in
-        body = (
-            <>
-                <NextLink href="/create-beat">
-                    <Button as={Link} mr={4} tabIndex={0}>
-                        create beat
-                    </Button>
-                </NextLink>
-                <NextLink href="/[username]" as={`/${data.me.username}`}>
-                    <Button variant="link">{data.me.username}</Button>
-                </NextLink>
-                <Button
-                    variant="link"
-                    isLoading={logoutFetching}
-                    onClick={() => {
-                        logout();
-                    }}
-                >
-                    logout
-                </Button>
-            </>
+            <NavLayoutWithAuth varient={sizeType} username={data.me.username} />
         );
     }
 
     return (
-        <Flex zIndex={1} position="sticky" top={0} bg="purple.800" p={4}>
+        <Flex
+            zIndex={1}
+            position="sticky"
+            top={0}
+            backgroundColor="purple.800"
+            p={4}
+        >
             <Flex maxWidth={1000} align="center" flex={1} m="auto">
                 <NextLink href="/">
-                    <Button variant="link" mr="auto">
+                    <Button color="white" variant="link" mr="auto">
                         <Heading>Beat Store</Heading>
                     </Button>
                 </NextLink>
-                <Flex
-                    justifyContent="center"
-                    alignItems="center"
-                    gridGap="1em"
-                    ml="auto"
-                >
-                    {body}
-                </Flex>
+                {body}
             </Flex>
         </Flex>
     );
