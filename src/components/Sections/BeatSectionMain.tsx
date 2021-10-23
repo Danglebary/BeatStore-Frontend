f7ff809581c379885286717c58cfbe0c7141790a
Danglebary <halfblowncontact@gmail.com>
2021-10-23 11:10:40 -0700

    updated stack to VStack component

--- src/components/Sections/BeatSectionMain.tsx
+++ src/components/Sections/BeatSectionMain.tsx
@@ -1,18 +1,18 @@
 // General imports
 import React from "react";
 // Chakra imports
-import { Stack } from "@chakra-ui/layout";
+import { VStack } from "@chakra-ui/layout";
 // GraphQL imports
 import { BeatMainFragment } from "../../generated/graphql";
 // Custom imports
 import { BeatCardMain } from "../Cards/BeatCardMain";
 import { AudioMain } from "../Audio/AudioMain";
 
-interface BeatSectionProps {
+interface Props {
     beats: BeatMainFragment[];
 }
 
-export const BeatSection: React.FC<BeatSectionProps> = ({ beats }) => {
+export const BeatSection: React.FC<Props> = ({ beats }) => {
     const beatAudioDatas: { url: string; title: string }[] = [];
     beats.forEach((beat) => {
         beatAudioDatas.push({ url: beat.s3Key, title: beat.title });
@@ -21,13 +21,13 @@ export const BeatSection: React.FC<BeatSectionProps> = ({ beats }) => {
     return (
         <>
             <AudioMain beats={beatAudioDatas} />
-            <Stack spacing={8}>
+            <VStack align="stretch" spacing={8} pb={200}>
                 {beats.map((beat) =>
                     !beat ? null : (
                         <BeatCardMain beat={beat} key={beat.title + beat.id} />
                     )
                 )}
-            </Stack>
+            </VStack>
         </>
     );
 };
